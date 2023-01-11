efafcfa6c497c1ea3444c6145e0f4768d1d92129
AaLin-Git <aalin.box.it@gmail.com>
2023-01-11 14:49:47 +0100

    init: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let str = String(Math.abs(n));
+    for (let i = 0; i < str.length; i++) {
+        result = `${str[i]}${result}`;
+    }
+    return Number(result);
 }
+
